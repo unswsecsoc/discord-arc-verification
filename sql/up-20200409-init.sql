f69a9b0cc8ba6e080b246e3f07c1395e16c4d73e
tom <hi@tomn.me>
2020-04-09 02:06:12 +1000

    add database schemas

---
+++ sql/up-20200409-init.sql
@@ -0,0 +1,41 @@
+CREATE TABLE users (
+	_id		SERIAL		PRIMARY KEY,
+	given_name	VARCHAR(64)	NOT NULL,
+	family_name	VARCHAR(64),
+	zid		CHAR(7),
+	discord_id	VARCHAR(20)	UNIQUE,
+	email		VARCHAR(255),
+	arc_member	BOOLEAN		NOT NULL,
+	phone		VARCHAR(16),
+	is_verified	BOOLEAN		NOT NULL DEFAULT FALSE,
+	created_at	TIMESTAMP	NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_at	TIMESTAMP	NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	deleted_at	TIMESTAMP,
+
+	CONSTRAINT 	zid_email_not_null CHECK (zid IS NOT NULL OR email IS NOT NULL),
+	CONSTRAINT	zid_phone_not_null CHECK (zid IS NOT NULL OR phone IS NOT NULL)
+);
+
+CREATE TABLE clubs (
+	_id			SERIAL		PRIMARY KEY,
+	name			VARCHAR(32)	NOT NULL,
+	email			VARCHAR(255)	NOT NULL,
+	admin_role_id		VARCHAR(20),
+	verified_role_id	VARCHAR(20),
+	discord_id		VARCHAR(20)	UNIQUE,
+	is_enabled		BOOLEAN		NOT NULL,
+	created_at		TIMESTAMP	NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_at		TIMESTAMP	NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	deleted_at		TIMESTAMP
+);
+
+CREATE TABLE members (
+	user_id		INTEGER		REFERENCES users(_id) ON DELETE CASCADE,
+	club_id		INTEGER		REFERENCES clubs(_id) ON DELETE CASCADE,
+	created_at	TIMESTAMP	NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	deleted_at	TIMESTAMP
+
+	UNIQUE(user_id, club_id)
+);
+
+CREATE TRIGGER update_timestamp BEFORE UPDATE ON users FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
