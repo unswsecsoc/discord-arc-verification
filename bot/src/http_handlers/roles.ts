dbf2334065b21c8771d5466fb4521e4662e74824
tom <hi@tomn.me>
2020-04-09 21:36:19 +1000

    deduplicate rpc calls

--- bot/src/http_handlers/roles.ts
+++ bot/src/http_handlers/roles.ts
@@ -1,6 +1,8 @@
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance,  } from 'fastify';
 import DiscordClient from '../services/discord';
 import { DiscordAPIError } from 'discord.js';
+import fastify = require('fastify');
+import { ServerResponse } from 'http';
 
 export default async function (fastify: FastifyInstance): Promise<void> {
 
@@ -24,57 +26,18 @@ export default async function (fastify: FastifyInstance): Promise<void> {
         required: ['user_id', 'role_ids']
     });
 
-    fastify.route({
-        method: 'POST',
-        url: '/add-roles',
-        schema: {
-            body: {
-                $ref: 'RoleController.user-role'
-            }
-        },
-        handler: async (request, reply) => {
+    function addRemoveRole(action: boolean) {
+        return async (request: fastify.FastifyRequest, reply: fastify.FastifyReply<ServerResponse>) => {
+            const {role_ids, guild_id, user_id} = request.body;
+
+            const guild = DiscordClient.guilds.resolve(guild_id);
+            if (!guild) return reply.status(400).send({error: 'GuildNotFound'});
+            const member = guild.members.resolve(user_id);
+            if (!member) return reply.status(400).send({error: 'MemberNotFound'});
+
             try {
-                const {role_ids, guild_id, user_id} = request.body;
-
-                const guild = DiscordClient.guilds.resolve(guild_id);
-                if (!guild) return reply.status(400).send({error: 'GuildNotFound'});
-                const member = guild.members.resolve(user_id);
-                if (!member) return reply.status(400).send({error: 'MemberNotFound'});
-                member.roles.add(role_ids);
-
-                reply.send({ data: true });
-            } catch (e) {
-                if (e instanceof DiscordAPIError) {
-                    reply.status(400).send({error: 'DiscordAPIError'});
-                    e.code
-                    // TODO: find admin channel and send error
-
-                    return;
-                }
-                reply.status(500).send({error: 'InternalServerError'});
-                throw e;
-            }
-        }
-    });
-
-    fastify.route({
-        method: 'POST',
-        url: '/remove-roles',
-        schema: {
-            body: {
-                $ref: 'RoleController.user-role'
-            }
-        },
-        handler: async (request, reply) => {
-            try {
-                const {role_ids, guild_id, user_id} = request.body;
-
-                const guild = DiscordClient.guilds.resolve(guild_id);
-                if (!guild) return reply.status(400).send({error: 'GuildNotFound'});
-                const member = guild.members.resolve(user_id);
-                if (!member) return reply.status(400).send({error: 'MemberNotFound'});
-                member.roles.remove(role_ids);
-
+                if (action) member.roles.add(role_ids);
+                else member.roles.remove(role_ids);
                 reply.send({ data: true });
             } catch (e) {
                 if (e instanceof DiscordAPIError) {
@@ -87,5 +50,27 @@ export default async function (fastify: FastifyInstance): Promise<void> {
                 throw e;
             }
         }
+    }
+
+    fastify.route({
+        method: 'POST',
+        url: '/add-roles',
+        schema: {
+            body: {
+                $ref: 'RoleController.user-role'
+            }
+        },
+        handler: addRemoveRole(true)
+    });
+
+    fastify.route({
+        method: 'POST',
+        url: '/remove-roles',
+        schema: {
+            body: {
+                $ref: 'RoleController.user-role'
+            }
+        },
+        handler: addRemoveRole(false)
     });
 }
